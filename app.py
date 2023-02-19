e4664a6ecae0c161e71a385d6af178c76c30328e
Darth Vader <danieltarancon@gmail.com>
2023-02-19 13:20:01 +0100

    request

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import tkinter as tk
 from tkinter import *
+from PIL import ImageTk, Image
+import requests
+import json
+from datetime import datetime
+from key import my_weather_key
 
+
+#Creating the GUI window
 root = tk.Tk()
-
+root.title("Weather App")
 root.geometry("1000x1000")
 
+
+
+# Creating the labels for the weather data and forecast
 
 location_label = Label(root, text="Location")
 location_label.pack()
@@ -18,6 +28,26 @@ wind_label = Label(root, text="Wind Speed")
 wind_label.pack()
 forecast_label = Label(root, text="Forecast")
 forecast_label.pack()
+
+
+#Making the API call to OpenWeatherMap
+
+api_key = my_weather_key
+city_name = input('Please enter the city name: ')
+#city_name = 'Rome'
+country_code = input('Please enter the country code: ')
+#country_code = 'IT'
+url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{country_code}&appid={api_key}'
+res = requests.get(url)
+print(res.json())
+
+
+image = Image.open('rain.jpeg')
+
+photo = ImageTk.PhotoImage(image)
+
+label = Label(root, image=photo)
+label.pack()
 
 
 
