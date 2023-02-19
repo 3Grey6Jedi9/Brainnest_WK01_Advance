921dda542c910577f6dad05c50b0869290a92d9d
Darth Vader <danieltarancon@gmail.com>
2023-02-19 01:48:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,29 @@
 import tkinter as tk
+from tkinter import *
 
 root = tk.Tk()
 
 root.geometry("1000x1000")
+
+
+location_label = Label(root, text="Location")
+location_label.pack()
+weather_label = Label(root, text="Weather")
+weather_label.pack()
+temperature_label = Label(root, text="Temperature")
+temperature_label.pack()
+humidity_label = Label(root, text="Humidity")
+humidity_label.pack()
+wind_label = Label(root, text="Wind Speed")
+wind_label.pack()
+forecast_label = Label(root, text="Forecast")
+forecast_label.pack()
+
+
+
+
+
+
+
 
 root.mainloop()
